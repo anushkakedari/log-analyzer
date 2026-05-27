a1ff49599bb8a79db531db6cbddf2b58c1c4d07a
anushkakedari <anushkakedari20@gmail.com>
2026-05-27 15:59:57 +0530

    Update CORS for Railway

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,10 @@ app = FastAPI(title="Log Analyzer API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://laudable-respect-production-f4f1.up.railway.app/",
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
