42919e9f1caf2e2b2d2dbaa55dfceb98f2d64d4a
anushkakedari <anushkakedari20@gmail.com>
2026-06-27 13:14:36 +0530

    Fix CORS for new Vercel URL

--- backend/main.py
+++ backend/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
         "http://localhost:5173",
         "https://laudable-respect-production-f4f1.up.railway.app",
         "https://log-analyzer-steel.vercel.app",
+        "https://log-analyzer-anushkakedaris-projects.vercel.app",
         ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -37,30 +38,3 @@ def health():
     return {"status": "ok"}
 
 
-
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from database import engine, Base
-# import models
-
-
-# app = FastAPI(title="Log Analyzer API", version="1.0.0")
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["http://localhost:5173"],  # React dev server
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
-
-# @app.get("/")
-# def root():
-#     return {"message": "Log Analyzer API is running 🚀"}
-
-# @app.get("/health")
-# def health():
-#     return {"status": "ok"}
-
-# # Create all tables on startup
-# Base.metadata.create_all(bind=engine)
